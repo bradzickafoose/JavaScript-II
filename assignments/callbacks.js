ba2a4545fbd2bd9629b30bf6a04ab609fc264666
Brad <brad@viziworx.com>
2019-09-17 18:52:19 -0400

    Finished callback.js minus the stretch. Working on array-methods.js. Still need to complete closure.js.

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -40,26 +40,70 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
-}
+  // getLength passes the length of the array into the callback. 
+return cb (arr.length);
+};
+getLength(items, function(length) {
+  console.log(length);
+})
+// console.log(getLength());
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
+// Option A:
+// last(items, function(lastItem) {
+//   console.log(lastItem);
+// });
+
+// Option B:
+last(items, (cb) => {
+	console.log(cb);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+return cb(x, y);
 }
+
+const add = (x,y) =>{
+  return x + y;
+}
+// console.log(add(1, 3));
+console.log(sumNums(4,4, add))
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x, y);
 }
+
+const multiply = (x,y) =>{
+  return x * y;
+};
+console.log(multiply(6,4));
+console.log(multiplyNums(6,4, multiply));
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if (list.includes(item)) {
+    return cb(true);
+  } else {
+    return cb(false);
+  }
 }
+console.log(items.includes('Pencil'));
 
+// Jasmine
+// const result = list;
+// 	if (result === list) {
+// 		return true;
+// 	} else {
+// 		return false;
+// 	}
+// };
+console.log(contains());
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
